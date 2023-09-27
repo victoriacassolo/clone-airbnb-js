125e399f2b72d58bf8d29d3047ec359887c7f467
Victoria Cassolo <vitoria.cassolo@gmail.com>
2023-09-27 16:54:44 -0300

    add prisma to db

--- app/components/modals/Modal.jsx
+++ app/components/modals/Modal.jsx
@@ -92,6 +92,7 @@ const Modal = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
