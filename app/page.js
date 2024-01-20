12c26c59020cf6839b3acf219f8b60deaa660422
victoriacassolo <vitoria.cassolo@gmail.com>
2024-01-20 20:04:34 -0300

    feat: init add fetch POST listings

--- app/page.js
+++ app/page.js
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello Airbnb!</div>;
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
+export default async function Home() {
+  const listings = await getListings();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
