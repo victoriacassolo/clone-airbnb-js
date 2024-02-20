667f8b9196e309e3cc202a5627f613777bd3862c
victoriacassolo <vitoria.cassolo@gmail.com>
2024-02-20 15:57:32 -0300

    chore: change name

--- app/page.jsx
+++ app/page.jsx
@@ -13,11 +13,14 @@ export default async function Home() {
       </ClientOnly>
     );
   }
+
   return (
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          <div>My future listings</div>
+          {listings.map((listing) => (
+            <div index={listing.id}>{listing.title}</div>
+          ))}
         </div>
       </Container>
     </ClientOnly>
