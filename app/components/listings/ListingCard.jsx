7e42829add4eac6d1635f8e79aed9de83444079a
victoriacassolo <vitoria.cassolo@gmail.com>
2024-02-24 16:23:52 -0300

    feat: create a card of listing cards

--- app/components/listings/ListingCard.jsx
+++ app/components/listings/ListingCard.jsx
@@ -2,29 +2,75 @@
 
 import useCountries from "@/app/hooks/useCountries";
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
+import { format } from "date-fns";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 const ListingCard = ({
   data,
   reservation,
   disabled,
   actionLabel,
-  actionId,
+  actionId = "",
   currentUser,
   onAction = () => {},
 }) => {
   const router = useRouter();
   const { getByValue } = useCountries();
   const location = getByValue(data?.locationValue);
-  const handleCancel = useCallback(() => {
-    if (disabled) {
-      return;
+  const handleCancel = useCallback(
+    (e) => {
+      e.stopPropagation();
+      if (disabled) {
+        return;
+      }
+
+      onAction?.(actionId);
+    },
+    [onAction, actionId, disabled]
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
     }
 
-    onAction?.(actionId);
-  }, [onAction, actionId]);
+    return data.price;
+  }, [reservation, data.price]);
 
-  return <div>Listing Card</div>;
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div
+      onClick={() => {
+        router.push(`/listings/${data?.id}`);
+      }}
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            fill
+            alt="Listing"
+            src={data?.imageSrc}
+            className="object-cover h-full w-full group-hover:scale-110 transition"
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data?.id} currentUser={currentUser} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
